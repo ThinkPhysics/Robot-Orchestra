534a233f7e663b73c1cc9d71543d16334ff88c86
Jonathan Sanderson <jonathan.sanderson@northumbria.ac.uk>
2016-10-06 12:56:48 +0100

    Refactor functions into imported module
    Significant clean-up, examples, commentary

---
+++ Robot_Orchestra_Huzzah/mod_orchestra.py
@@ -0,0 +1,55 @@
+"""Robot orchestra abstractions.
+
+Provides a simple language grammar for issuing beat patterns and playback
+cue to Robot Orchestra instruments, and handles MQTT messaging.
+"""
+import paho.mqtt.client as mqtt
+import time
+
+mqttc = mqtt.Client()
+mqtt_server = "10.0.1.4"
+
+instruments = {"00": "skutter_18:FE:34:FD:91:AD",
+               "01": "skutter_5C:CF:7F:01:5B:22",
+               "02": "skutter_18:FE:34:FD:92:D1",
+               "03": "skutter_18:FE:34:F4:D3:BD",
+               "04": "skutter_18:FE:34:FD:93:33"
+               }
+
+
+def message(topic, payload):
+    """Abstract out MQTT connection.
+
+    Since it has to be done for each message, wrap it in a function.
+    """
+    mqttc.connect(mqtt_server, 1883)
+    mqttc.publish("orchestra/" + topic, payload)
+    # time.sleep(0.2)
+
+
+def set_active(robots):
+    """Target specific robot(s) so they respond to subsequent commands."""
+    # print "Setting active: "
+    # print robots
+    # First set all robots inactive...
+    for instrument in instruments:
+        message(instruments[instrument], 0)
+    # ...now enable just the target robots
+    time.sleep(0.2)
+    for robot in robots:
+        # print robot
+        message(instruments[robot], 1)
+        # print "Instrument key:" + robot
+        # print "Instrument value:" + instruments[robot]
+    time.sleep(0.2)
+
+
+def beats(pattern):
+    """Send beat pattern to robots."""
+    # print "Beats: " + pattern
+    message("beats", pattern)
+
+
+def play():
+    """Issue the play command."""
+    message("play", 1)
--- Robot_Orchestra_Huzzah/robot_orchestra.py
+++ Robot_Orchestra_Huzzah/robot_orchestra.py
@@ -2,55 +2,9 @@
 
 Distributes and commands network of robots to play instruments.
 """
-import paho.mqtt.client as mqtt
 import time
+from mod_orchestra import set_active, beats, play
 
-mqttc = mqtt.Client()
-
-instruments = {"00": "skutter_18:FE:34:FD:91:AD",
-               "01": "skutter_5C:CF:7F:01:5B:22",
-               "02": "skutter_18:FE:34:FD:92:D1",
-               "03": "skutter_18:FE:34:F4:D3:BD",
-               "04": "skutter_18:FE:34:FD:93:33"
-               }
-
-
-def message(topic, payload):
-    """Abstract out MQTT connection.
-
-    Since it has to be done for each message, wrap it in a function.
-    """
-    mqttc.connect('localhost', 1883)
-    mqttc.publish("orchestra/" + topic, payload)
-    # time.sleep(0.2)
-
-
-def set_active(robots):
-    """Target specific robot(s) so they respond to subsequent commands."""
-    # print "Setting active: "
-    # print robots
-    # First set all robots inactive...
-    for instrument in instruments:
-        message(instruments[instrument], 0)
-    # ...now enable just the target robots
-    time.sleep(0.2)
-    for robot in robots:
-        # print robot
-        message(instruments[robot], 1)
-        # print "Instrument key:" + robot
-        # print "Instrument value:" + instruments[robot]
-    time.sleep(0.2)
-
-
-def beats(pattern):
-    """Send beat pattern to robots."""
-    # print "Beats: " + pattern
-    message("beats", pattern)
-
-
-def play():
-    """Issue the play command."""
-    message("play", 1)
 
 # Set up instrument groups
 all = ("00", "01", "02", "03", "04")
@@ -66,18 +20,22 @@ drums = ("00", "04")
 # First, make sure the beat pattern is zeroed out for all instruments
 set_active(all)
 beats("0000")
+time.sleep(0.2)
+
+# Here's a pattern we're going to reuse
+closing = "1010011"
 
 # Now configure pattern for each instrument
 set_active(zero)
-beats("101000000000000000000101")
+beats("10100000000000000000" + closing)
 set_active(one)
-beats("000010100000000000000101")
+beats("00001010000000000000" + closing)
 set_active(two)
-beats("000000001010000000000101")
+beats("00000000101000000000" + closing)
 set_active(three)
-beats("000000000000101000000101")
+beats("00000000000010100000" + closing)
 set_active(four)
-beats("000000000000000010100101")
+beats("00000000000000001010" + closing)
 
 # ...and finally command all instruments to play at once
 set_active(all)
