b51d3414d808e0d5377603120611d9e0336789e9
Joe <joeshimwell@gmail.com>
2016-10-28 15:33:38 +0000

    Twinkle Twinkle Little Star

--- Robot_Orchestra_Huzzah/Twitch_program.py
+++ Robot_Orchestra_Huzzah/Twitch_program.py
@@ -1,8 +1,15 @@
+'''This small program provide an easy way to test the robots, by causing them
+to 'twitch' once every five seconds. Change which robots move by adding
+number "01" to "11" into 'ROBOTS'
+Cancel their twitching by pressing control-C in the Terminal.
+'''
 import time
-from mod_orchestra import twitch, set_inactive
+from mod_orchestra import twitch, send_beats
 
-ROBOTS = ("01","02","03","04","05","00","11")
-stop = "n"
-while  stop == "n":
+ROBOTS = ("06","09")
+#clears the robots current beat pattern
+send_beats(ROBOTS, "0")
+#tells the robots to beat once very five seconds
+while  True == True:
     twitch(ROBOTS)
     time.sleep (5)
--- Robot_Orchestra_Huzzah/robot_orchestra.py
+++ Robot_Orchestra_Huzzah/robot_orchestra.py
@@ -8,38 +8,41 @@ from mod_orchestra import send_beats, set_active, set_inactive, play
 
 # Set up instrument groups
 ALL = ("00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11")
-ZERO = ("00",)
-ONE = ("01",)
-TWO = ("02",)
-THREE = ("03",)
-FOUR = ("04",)
-FIVE = ("05",)
-SIX = ("06",)
-SEVEN = ("07",)
-EIGHT = ("08",)
-NINE = ("09",)
-TEN = ("10",)
-ELEVEN = ("11",)
-# add your own groups here, for example:
-DRUMS = ("00","11")
-
-# Here are patterns we're going to reuse
-CLOSING = "01"
-BEAT_1 = "1000"
-BEAT_2 = "0100"
-BEAT_3 = "0010"
-BEAT_4 = "0001"
-# You could use similar things to define a chorus, an intro, and so on.
-
-#Remember to clear the beat patterns before starting!
+#clears the robots' beat patterns
 send_beats(ALL, "0")
+# These could include things like a 'drums' section
+'''This dictionary holds the names and coreesponding numbers of each of the
+instruments'''
+PLAYERS = {"ZERO": ("00",),
+       "ONE": ("01",),
+       "TWO": ("02",),
+       "THREE": ("03",),
+       "FOUR": ("04",),
+       "FIVE": ("05",),
+       "SIX": ("06",),
+       "SEVEN": ("07",),
+       "EIGHT": ("08",),
+       "NINE": ("09",),
+       "TEN": ("10",),
+       "ELEVEN": ("11",),
+       }
 
-# Now configure pattern for each instrument
-send_beats(ONE, BEAT_1 + CLOSING + BEAT_4 + CLOSING + "1000100010001")
-send_beats(TWO, BEAT_2 + CLOSING + BEAT_3 + CLOSING + "0010000000101")
-send_beats(THREE, BEAT_3 + CLOSING + BEAT_2 + CLOSING + "0101001001011")
-send_beats(FOUR, BEAT_4 + CLOSING + BEAT_1 + CLOSING + "0000010100001")
-# ... and so on
+#Opens a seperate text file and uses that to send beat patterns to the instruments
+with open('TwinkleTwinkle.txt', 'r') as music:
+    total = music.readlines()
+# splits the txt file into smaller and smaller fragments
+    for line in total:
+        beats = line.split(":")
+        robot = (beats[0]).replace(':','')
+        print robot
+        pattern = (beats[1]).replace(' ', '')
+        pattern = pattern.replace('\n','')
+# Compares the number in the txt file with the items in the dictionary 'players'
+        for number in PLAYERS:
+            if robot == number:
+                robo_num = PLAYERS[number]
+# sends the beat pattern to a specific robot
+        send_beats(robo_num,pattern)
 
-# Finally, command all instruments to play at once
+#Sets all of the instruments to play
 play(ALL)
